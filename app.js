16970355bbd7faf6fbae44a36c985906a548ef77
CodewellTech <codequicklearner@gmail.com>
2022-01-27 11:00:14 +0530

    latest videos updated
    latest videos updated

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-let container=document.querySelector(".container");
+let container = document.querySelector(".container");
 function loadData() {
-    const response = fetch("data.json");
-    response.then(function (res) {
-        res.json().then(function (data) {
-            data=data?.reverse() ||[];
-            setTimeout(() => {
-                renderDataOnPage(data);
-            }, 2000);            
-        })
+  const response = fetch("data.json");
+  response.then(function (res) {
+    res.json().then(function (data) {
+      // data=data?.reverse() ||[];
+      setTimeout(() => {
+        renderDataOnPage(data);
+      }, 2000);
     });
-};
+  });
+}
 loadData();
 let df = document.createDocumentFragment();
 function renderDataOnPage(data = []) {
-    data.forEach((data) => {
-        let item=document.createElement("div");
-        item.classList.add("item");
-        let infoStr=" ";
-        data.info.forEach(function(info){                        
-            infoStr+=(`<li>${info}</li>`);                        
-        });
-        str = `       
+  data.forEach((data) => {
+    let item = document.createElement("div");
+    item.classList.add("item");
+    let infoStr = " ";
+    data.info.forEach(function (info) {
+      infoStr += `<li>${info}</li>`;
+    });
+    str = `       
                 <div class="title">
                     <h2>${data.title}</h2>
                 </div>
@@ -36,14 +36,14 @@ function renderDataOnPage(data = []) {
                     <iframe src="${data.url}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
                     </iframe>
                 </div>       
-             `
-        item.insertAdjacentHTML("beforeend",str);
-        item.querySelector("button").disabled=!data.sc;
-        df.append(item);
-    });
-    container.append(df);
-    document.querySelector(".loading").style.display="none";
-    container.style.opacity=1;
+             `;
+    item.insertAdjacentHTML("beforeend", str);
+    item.querySelector("button").disabled = !data.sc;
+    df.append(item);
+  });
+  container.append(df);
+  document.querySelector(".loading").style.display = "none";
+  container.style.opacity = 1;
 }
 
 // window.addEventListener("resize",function(){
